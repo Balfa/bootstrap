f69733834f91a62d2c74a6d0410f37a845a9c459
Mo <Balfa@users.noreply.github.com>
2025-02-10 19:02:54 -0600

    boxstarter-administered-appliances.txt & boxstarter-personal.txt

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,6 +1,9 @@
 Set-ExecutionPolicy RemoteSigned
 . { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; Get-Boxstarter -Force
 iex "& { $(iwr -useb https://raw.githubusercontent.com/Balfa/bootstrap/refs/heads/mehhacking/regfixes.ps1) } -ComputerName Weggles"
-. { iwr -useb https://raw.githubusercontent.com/Balfa/bootstrap/refs/heads/mehhacking/regfixes.ps1 } | iex;
-Install-BoxstarterPackage -DisableReboots -PackageName https://gist.githubusercontent.com/Balfa/d39406f11e067aca19104783ed5e45aa/raw/boxstarter-p.txt
+Install-BoxstarterPackage -DisableReboots -PackageName https://raw.githubusercontent.com/Balfa/bootstrap/refs/heads/mehhacking/boxstarter-administered-appliances.txt
+# Stop here if this install is just for family member PCs etc.
+Install-BoxstarterPackage -DisableReboots -PackageName https://raw.githubusercontent.com/Balfa/bootstrap/refs/heads/mehhacking/boxstarter-personal.txt
+# Stop here if it's for any of my personal PCs other than my primary desktop
 # Install-BoxstarterPackage -DisableReboots -PackageName https://gist.githubusercontent.com/Balfa/d39406f11e067aca19104783ed5e45aa/raw/z-boxstarter-hardware-specific.txt
+Set-ExecutionPolicy Restricted
