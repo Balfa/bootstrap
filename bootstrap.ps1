ca5a5a68353fbadf374d607e1c9b48dbf13eb4c9
Mo <Balfa@users.noreply.github.com>
2025-02-10 18:38:18 -0600

    successfully call regfixes from new location AND WITH PARAMETER

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,5 +1,6 @@
 Set-ExecutionPolicy RemoteSigned
 . { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; Get-Boxstarter -Force
-. { iwr -useb https://git.githubusercontent.com/Balfa/bootstrap/raw/regfixes.ps1 } | iex;
+iex "& { $(iwr -useb https://raw.githubusercontent.com/Balfa/bootstrap/refs/heads/mehhacking/regfixes.ps1) } -ComputerName Weggles"
+. { iwr -useb https://raw.githubusercontent.com/Balfa/bootstrap/refs/heads/mehhacking/regfixes.ps1 } | iex;
 Install-BoxstarterPackage -DisableReboots -PackageName https://gist.githubusercontent.com/Balfa/d39406f11e067aca19104783ed5e45aa/raw/boxstarter-p.txt
 # Install-BoxstarterPackage -DisableReboots -PackageName https://gist.githubusercontent.com/Balfa/d39406f11e067aca19104783ed5e45aa/raw/z-boxstarter-hardware-specific.txt
