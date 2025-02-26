5adc402eb7d1be7bf363c7bf00c8c62df49cbaa8
Mo <Balfa@users.noreply.github.com>
2025-02-25 21:20:34 -0600

    split bootstrap in twain

--- README.md
+++ README.md
@@ -2,8 +2,15 @@
 scripts to bootstrap fresh windows install
 
 # Instructions
-1. Run powershell and paste contents of `bootstrap.ps1`.
+1. Run powershell (`wt`) as admin and paste the following:
+    ```ps1 
+    Set-ExecutionPolicy RemoteSigned
+    iex "& { $(iwr -useb https://raw.githubusercontent.com/Balfa/bootstrap/refs/heads/mehhacking/bootstrap.ps1) } -ComputerName Weggles -PackageLevel 'personal'"
+    Set-ExecutionPolicy Restricted
+    ```
 
 # TODO
 - Make it even more unattended. Single paste raw github file?
 - Start migrating apps from choco to winget?
+- Maybe split personal into gaming, dev, etc.?
+- The rest of postinstallfirststeps...
--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,9 +1,28 @@
-Set-ExecutionPolicy RemoteSigned
+param (
+    [string]$ComputerName,
+    [string]$PackageLevel
+)
+
 . { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; Get-Boxstarter -Force
-iex "& { $(iwr -useb https://raw.githubusercontent.com/Balfa/bootstrap/refs/heads/mehhacking/regfixes.ps1) } -ComputerName Weggles"
-Install-BoxstarterPackage -DisableReboots -PackageName https://raw.githubusercontent.com/Balfa/bootstrap/refs/heads/mehhacking/boxstarter-administered-appliances.txt
-# Stop here if this install is just for family member PCs etc.
-Install-BoxstarterPackage -DisableReboots -PackageName https://raw.githubusercontent.com/Balfa/bootstrap/refs/heads/mehhacking/boxstarter-personal.txt
-# Stop here if it's for any of my personal PCs other than my primary desktop
-Install-BoxstarterPackage -DisableReboots -PackageName https://raw.githubusercontent.com/Balfa/bootstrap/refs/heads/mehhacking/boxstarter-hardware-specific.txt
-Set-ExecutionPolicy Restricted
+iex "& { $(iwr -useb https://raw.githubusercontent.com/Balfa/bootstrap/refs/heads/mehhacking/regfixes.ps1) } -ComputerName $ComputerName"
+
+switch ($PackageLevel) {
+    "administered" {
+        # just for family member PCs etc.
+        Install-BoxstarterPackage -DisableReboots -PackageName https://raw.githubusercontent.com/Balfa/bootstrap/refs/heads/mehhacking/boxstarter-administered-appliances.txt
+    }
+    "personal" {
+        # for any of my personal PCs other than my primary desktop
+        Install-BoxstarterPackage -DisableReboots -PackageName https://raw.githubusercontent.com/Balfa/bootstrap/refs/heads/mehhacking/boxstarter-administered-appliances.txt
+        Install-BoxstarterPackage -DisableReboots -PackageName https://raw.githubusercontent.com/Balfa/bootstrap/refs/heads/mehhacking/boxstarter-personal.txt
+    }
+    "hardware-specific" {
+        # for my primary desktop (SteelSeries Engine, etc.)
+        Install-BoxstarterPackage -DisableReboots -PackageName https://raw.githubusercontent.com/Balfa/bootstrap/refs/heads/mehhacking/boxstarter-administered-appliances.txt
+        Install-BoxstarterPackage -DisableReboots -PackageName https://raw.githubusercontent.com/Balfa/bootstrap/refs/heads/mehhacking/boxstarter-personal.txt
+        Install-BoxstarterPackage -DisableReboots -PackageName https://raw.githubusercontent.com/Balfa/bootstrap/refs/heads/mehhacking/boxstarter-hardware-specific.txt
+    }
+    default {
+        Write-Error "Invalid PackageLevel specified. Use 'administered', 'personal', or 'hardware-specific'."
+    }
+}
