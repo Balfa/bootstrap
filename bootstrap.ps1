9965802a10fdc9ca191d654fef9b3130c70295db
Mo <Balfa@users.noreply.github.com>
2025-02-25 20:29:52 -0600

    better hw specific

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -5,5 +5,5 @@ Install-BoxstarterPackage -DisableReboots -PackageName https://raw.githubusercon
 # Stop here if this install is just for family member PCs etc.
 Install-BoxstarterPackage -DisableReboots -PackageName https://raw.githubusercontent.com/Balfa/bootstrap/refs/heads/mehhacking/boxstarter-personal.txt
 # Stop here if it's for any of my personal PCs other than my primary desktop
-# Install-BoxstarterPackage -DisableReboots -PackageName https://gist.githubusercontent.com/Balfa/d39406f11e067aca19104783ed5e45aa/raw/z-boxstarter-hardware-specific.txt
+Install-BoxstarterPackage -DisableReboots -PackageName https://raw.githubusercontent.com/Balfa/bootstrap/refs/heads/mehhacking/boxstarter-hardware-specific.txt
 Set-ExecutionPolicy Restricted
