60bb003fd35bea10803d876b7573aa8abc063678
Mo <Balfa@users.noreply.github.com>
2025-02-28 14:07:59 -0600

    moving some of my remote github bash scripts in here

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -23,11 +23,21 @@ Install-BoxstarterPackage -DisableReboots -PackageName $administeredPackage
 # Install personal package for 'personal' or 'hardware-specific' levels
 if ($PackageLevel -in @("personal", "hardware-specific")) {
     Install-BoxstarterPackage -DisableReboots -PackageName $personalPackage
+    # dotfiles (bash, git...)
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Balfa/dotfiles/main/install.sh" -UseBasicParsing | Select-Object -ExpandProperty Content | & "C:\Program Files\Git\bin\bash.exe"
-
+    ## to test...
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Balfa/flumpus/main/test_gitconfig.sh" -UseBasicParsing | Select-Object -ExpandProperty Content | & "C:\Program Files\Git\bin\bash.exe"
+    # Install my Sublime timestamp script
+    Invoke-WebRequest -Uri "https://gist.githubusercontent.com/Balfa/e56e6c1fffcb8609828ab637942dbaa7/raw/install.sh" -UseBasicParsing | Select-Object -ExpandProperty Content | & "C:\Program Files\Git\bin\bash.exe"
+    # Add "Open With Sublime Text" to context menu (with icon!!)
+    Invoke-Expression "& { $(Invoke-WebRequest -useb https://gist.githubusercontent.com/Balfa/3a7e049dc1ac2d8ae1441a6e4e7c3f37/raw/OpenWithSublimeText.bat) }"
 }
 
 # Install hardware-specific package only for 'hardware-specific' level
 if ($PackageLevel -eq "hardware-specific") {
     Install-BoxstarterPackage -DisableReboots -PackageName $hardwareSpecificPackage
+    # Install RDP Pass To Local
+    Invoke-Expression "& { $(Invoke-WebRequest -useb https://raw.githubusercontent.com/Balfa/RdpPassToLocal/main/install.cmd) }"
+    # And add soundswitch to startup
+    Invoke-Expression "& { $(Invoke-WebRequest -useb https://raw.githubusercontent.com/Balfa/RdpPassToLocal/main/add-soundswitch-to-startup.cmd) }"
 }
