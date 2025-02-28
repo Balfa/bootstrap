470b78a0d18275af7ec9ccb301ef8f7c67d9410a
Mo <Balfa@users.noreply.github.com>
2025-02-28 14:29:33 -0600

    whoops. cmd scripts won't actually work in PS

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -29,14 +29,14 @@ if ($PackageLevel -in @("personal", "hardware-specific")) {
     # Install my Sublime timestamp script
     Invoke-WebRequest -Uri "https://gist.githubusercontent.com/Balfa/e56e6c1fffcb8609828ab637942dbaa7/raw/install.sh" -UseBasicParsing | Select-Object -ExpandProperty Content | & "C:\Program Files\Git\bin\bash.exe"
     # Add "Open With Sublime Text" to context menu (with icon!!)
-    Invoke-Expression "& { $(Invoke-WebRequest -useb https://gist.githubusercontent.com/Balfa/3a7e049dc1ac2d8ae1441a6e4e7c3f37/raw/OpenWithSublimeText.bat) }"
+    Invoke-WebRequest -Uri "https://gist.githubusercontent.com/Balfa/3a7e049dc1ac2d8ae1441a6e4e7c3f37/raw/OpenWithSublimeText.bat" -UseBasicParsing | Select-Object -ExpandProperty Content | & cmd
 }
 
 # Install hardware-specific package only for 'hardware-specific' level
 if ($PackageLevel -eq "hardware-specific") {
     Install-BoxstarterPackage -DisableReboots -PackageName $hardwareSpecificPackage
     # Install RDP Pass To Local
-    Invoke-Expression "& { $(Invoke-WebRequest -useb https://raw.githubusercontent.com/Balfa/RdpPassToLocal/main/install.cmd) }"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Balfa/RdpPassToLocal/main/install.cmd" -UseBasicParsing | Select-Object -ExpandProperty Content | & cmd
     # And add soundswitch to startup
-    Invoke-Expression "& { $(Invoke-WebRequest -useb https://raw.githubusercontent.com/Balfa/RdpPassToLocal/main/add-soundswitch-to-startup.cmd) }"
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/Balfa/RdpPassToLocal/main/add-soundswitch-to-startup.cmd" -UseBasicParsing | Select-Object -ExpandProperty Content | & cmd
 }
